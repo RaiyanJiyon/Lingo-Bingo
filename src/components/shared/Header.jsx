429f46d3cf5d49811941b526a7fa68c3df84f525
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-11-20 15:43:04 +0600

    feat: implement navbar in MainLayout - Added Navbar to MainLayout.jsx - Integrated Navbar with existing layout structure - Ensured responsive design and functionality

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,7 +1,46 @@
+import { Link, NavLink } from "react-router-dom";
+
 const Header = () => {
     return (
-        <div>
-            
+        <div className="navbar bg-[#1e0e5c] text-white">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 6h16M4 12h8m-8 6h16" />
+                        </svg>
+                    </div>
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                        <NavLink to={"/"} className={({ isActive }) => isActive ? 'text-black font-bold underline' : 'text-black font-normal'}>Home</NavLink>
+                        <NavLink to={"/start-learning"} className={({ isActive }) => isActive ? 'text-black font-bold underline' : 'text-black font-normal'}>Start Learning</NavLink>
+                        <NavLink to={"/tutorials"} className={({ isActive }) => isActive ? 'text-black font-bold underline' : 'text-black font-normal'}>Tutorials</NavLink>
+                        <NavLink to={"/about-us"} className={({ isActive }) => isActive ? 'text-black font-bold underline' : 'text-black font-normal'}>About Us</NavLink>
+                    </ul>
+                </div>
+                <a className="btn btn-ghost text-xl">Lingo Bingo</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1 gap-4">
+                    <NavLink to={"/"} className={({ isActive }) => isActive ? 'font-bold underline' : 'font-normal'}>Home</NavLink>
+                    <NavLink to={"/start-learning"} className={({ isActive }) => isActive ? 'font-bold underline' : 'font-normal'}>Start Learning</NavLink>
+                    <NavLink to={"/tutorials"} className={({ isActive }) => isActive ? 'font-bold underline' : 'font-normal'}>Tutorials</NavLink>
+                    <NavLink to={"/about-us"} className={({ isActive }) => isActive ? 'font-bold underline' : 'font-normal'}>About Us</NavLink>
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <Link to={"/auth/login"} className="px-4 py-1 rounded-3xl bg-[#1e0e5c] text-white border-2 border-[#7461be]">Sign In</Link>
+            </div>
         </div>
     );
 };
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,7 +1,19 @@
+import { Outlet } from "react-router-dom";
+import Header from "../components/shared/Header";
+import Footer from "../components/shared/Footer";
+
 const MainLayout = () => {
     return (
-        <div>
-            
+        <div className="w-11/12 2xl:w-4/5 mx-auto">
+            <div className="mt-6 sticky top-0 z-10">
+                <Header />
+            </div>
+            <div>
+                <Outlet />
+            </div>
+            <div>
+                <Footer />
+            </div>
         </div>
     );
 };
