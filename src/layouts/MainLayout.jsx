3f064863f18b6a1e24c6c35d13300f06f59ac7a0
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-11-21 00:02:40 +0600

    feat: implement footer in MainLayout - Added Footer to MainLayout.jsx - Integrated Foote with existing layout structure - Ensured responsive design and functionality

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,11 +4,11 @@ import Footer from "../components/shared/Footer";
 
 const MainLayout = () => {
     return (
-        <div className="w-11/12 2xl:w-4/5 mx-auto">
-            <div className="mt-6 sticky top-0 z-10">
+        <div className='w-11/12 2xl:w-4/5 mx-auto'>
+            <div className='mt-6 sticky top-0 z-10'>
                 <Header />
             </div>
-            <div>
+            <div className='min-h-[calc(100vh-505px)]'>
                 <Outlet />
             </div>
             <div>
