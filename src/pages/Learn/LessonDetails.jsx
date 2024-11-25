3c52dc0018ebaad421a02f58f0a8d8fb25d7ae62
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-11-25 21:43:02 +0600

    feat(pages): Implement word voice functionalities in LessonDetails cards

--- src/pages/Learn/LessonDetails.jsx
+++ src/pages/Learn/LessonDetails.jsx
@@ -26,20 +26,26 @@ const LessonDetails = () => {
         setIsModalOpen(false);
     };
 
+    const pronounceWord = (word) => {
+        const utterance = new SpeechSynthesisUtterance(word);
+        utterance.lang = 'ja-JP'; // Japanese
+        window.speechSynthesis.speak(utterance);
+    }
+
     return (
         <div className="mt-8">
             <h2 data-aos="zoom-in" className="text-3xl font-bold text-center">Lesson {lessonId} Details</h2>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-8">
                 {lesson.map(vocabulary => (
-                    <div key={vocabulary.id} className="card bg-base-100 shadow-xl transition-transform duration-300 ease-in-out transform hover:scale-110">
+                    <div key={vocabulary.id} onClick={() => pronounceWord(vocabulary.word)} className="card bg-base-100 shadow-xl transition-transform duration-300 ease-in-out transform hover:scale-110">
                         <div className="card-body">
                             <h2 className="card-title">{vocabulary.word}</h2>
                             <p><span className="font-bold">Meaning:</span> {vocabulary.meaning}</p>
                             <p><span className="font-bold">Pronunciation:</span> {vocabulary.pronunciation}</p>
                             <p><span className="font-bold">Part of Speech:</span> {vocabulary.part_of_speech}</p>
                             <div className="card-actions justify-end">
-                                <button 
-                                    className="btn w-full mt-4 bg-[#1e0e5c] text-white font-bold" 
+                                <button
+                                    className="btn w-full mt-4 bg-[#1e0e5c] text-white font-bold"
                                     onClick={() => handleViewMoreButton(vocabulary.word, vocabulary.meaning, vocabulary.when_to_say, vocabulary.example)}
                                 >
                                     When to Say
@@ -55,8 +61,8 @@ const LessonDetails = () => {
             </Link>
 
             {isModalOpen && (
-                <WhenToSayModal 
-                    isOpen={isModalOpen} 
+                <WhenToSayModal
+                    isOpen={isModalOpen}
                     closeModal={closeModal}
                     word={modalData.word}
                     meaning={modalData.meaning}
